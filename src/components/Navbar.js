164085c1a6bfa25a85f62bd1d87b10c9291dd59e
Todd M Brockman <todd.brockman@gmail.com>
2021-01-17 15:15:01 -0800

    Adjusting Logo sass

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="My Positive Outlooks" style={{ height: '100px'}} />
+              <img src={logo} alt="My Positive Outlooks" style={{ width: '250px'}} />
             </Link>
             {/* Hamburger menu */}
             <div
